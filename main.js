bee0cf4ae6ac8fbe11a177899bcd58585babaec2
Sandra Guðrún Guðmundsdóttir <sandragudrun@Sandras-MacBook-Pro.local>
2018-10-09 23:50:34 +0000

    verkefni 1

---
+++ main.js
@@ -0,0 +1,9 @@
+var TveirStrengir = "Svaka"+"stuð";
+var FirstNumber = 7-5*(8+1)/2;
+var firstBoolean = 5>1 && 5<7 || !(6>4);
+var secondBoolean = (5>4 && 6<4 && 5=="5") || 5==="5";  
+var thirdBoolean = 5!==6 && 5<=8 && 5>=5;
+var svar6 = "Breytan inniheldur gildið True af því breytan aboutKalli er samasem Kalli er góður strákur";
+var svar7 = "Breytan inniheldur gildið false af því fyrri breytan er sönn en sú seinni er false og báðar breytur þurfa að vera true til að breytan sé true af því það er & merki á milli";
+
+
